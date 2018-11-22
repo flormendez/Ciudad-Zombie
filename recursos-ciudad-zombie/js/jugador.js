6ddd52b48ad269fcc2ed495a5947ac0ae6e527f7
Florencia <florenciamendez93@gmail.com>
2018-11-22 19:07:53 -0300

    Ciudad Zombie

---
+++ recursos-ciudad-zombie/js/jugador.js
@@ -0,0 +1,38 @@
+/* El objeto jugador es un objeto literal que se encuentra incompleto.
+ Solo tiene asignadas algunas de sus propiedades y ningun metodo */
+var Jugador = {
+  /* el sprite contiene la ruta de la imagen
+  */
+  sprite: 'imagenes/auto_rojo_abajo.png',
+  x: 130,
+  y: 160,
+  ancho: 15,
+  alto: 30,
+  velocidad: 10,
+  vidas: 5,  
+  
+  // Hay que agregar lo que falte al jugador: movimientos, perdida de vidas,
+  // y todo lo que haga falta para que cumpla con sus responsabilidades
+  cambiarSprite: function (direccion) {
+    this.sprite = 'imagenes/auto_rojo_' + direccion + '.png';
+    if(direccion=='izquierda'||direccion=='derecha'){
+      this.ancho = 30;
+      this.alto = 15;
+    } 
+    if(direccion=='arriba'||direccion=='abajo'){
+      this.ancho = 15;
+      this.alto = 30;
+    } 
+  },
+
+  moverse: function (movX, movY) {
+    this.x = this.x + movX;
+    this.y = this.y + movY;
+  },
+  perderVidas: function (potencia) {
+    this.vidas=this.vidas-potencia;
+  }
+}
+  
+
+
---
+++ recursos-ciudad-zombie/js/zombie_conductor.js
@@ -0,0 +1,42 @@
+/* Para insipirarte para la implementacion del ZombieConductor podes usar
+al ZombieCaminante de ejemplo. Tene en cuenta que tendra algunas diferencias.
+Por ejemplo, la cantidad parametros que recibe su constructor. En ZombieConductor
+no son exactamente los mismos parametros que en el objeto Enemigo, a diferencia
+del ZombieCaminante que eran los mismos. */
+
+
+var ZombieConductor = function (sprite, x, y, ancho, alto, velocidad, rangoMov, direccion/*, parametro/s extra de ZombieConductor*/) {
+  /* Completar constructor a partir de Enemigo */
+  //Enemigo.call(/* ... */);
+  /* No olvidar agregar la/s propiedad/es unicas de ZombieConductor necesarias */
+  Enemigo.call(this, sprite, x, y, ancho, alto, velocidad, rangoMov);
+  this.direccion = direccion;
+}
+
+/* Completar creacion del ZombieConductor */
+ZombieConductor.prototype = Object.create(Enemigo.prototype);
+ZombieConductor.prototype.constructor = ZombieConductor;
+/* Completar metodos para el movimiento y el ataque */
+
+ZombieConductor.prototype.mover = function (direccion) {
+
+  if (this.direccion == "vertical") {
+    this.y += this.velocidad;
+  }
+  if (this.direccion == "horizontal") {
+    this.x += this.velocidad;
+  }
+
+  /* Reinicia posición cuando llega al borde*/
+  if ((this.x < this.rangoMov.desdeX) || (this.x > this.rangoMov.hastaX)) {
+    this.x = this.rangoMov.desdeX;
+  }
+  if ((this.y < this.rangoMov.desdeY) || (this.y > this.rangoMov.hastaY)) {
+    this.velocidad > 0 ? this.y = this.rangoMov.desdeY : this.y = this.rangoMov.hastaY;
+  }
+}
+
+ZombieConductor.prototype.atacar = function (jugador) {
+
+  jugador.perderVidas(jugador.vidas);
+}
